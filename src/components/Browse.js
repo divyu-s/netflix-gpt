7bc636f292b139eb0e2573fceaa9439e08ea91ea
Divyanshu Shukla <shukladivyanshu745893@gmail.com>
2024-04-24 12:50:55 +0530

    created MainContainer, VideoTitle, VideoBackground components

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
 
 const Browse = () => {
   useNowPlayingMovies();
 
   return (
-    <div>
+    <div className="relative">
       <Header />
-      <div>Browse</div>
+      <MainContainer />
     </div>
   );
 };
